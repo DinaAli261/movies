e0fd8211497c85d76a337c61003d5e36d864f016
dina ali <alidina275@gamil.com>
2025-11-04 11:45:25 +0200

    +initializing project

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color yellow = Color(0xFFF6BD00);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color white60 = Color(0x99FFFFFF);
+  static const Color black = Color(0xFF121312);
+  static const Color grey = Color(0xFF282A28);
+  static const Color lightGrey = Color(0xFFADADAD);
+  static const Color red = Color(0xFFE82626);
+  static const Color transparent = Colors.transparent;
+}
