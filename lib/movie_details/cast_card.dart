10dd35752e6f3e5b7055dea3115e60c569ab7c02
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2025-11-23 21:47:08 +0200

    + feature reusable cast card

--- lib/movie_details/cast_card.dart
+++ lib/movie_details/cast_card.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:movies/l10n/app_localizations.dart';
 import 'package:movies/utils/app_colors.dart';
-import 'package:movies/utils/app_images.dart';
 import 'package:movies/utils/app_text_styles.dart';
 
 class CastCard extends StatelessWidget {
-  final Widget img;
+  final String imgUrl;
   final String name;
   final String role;
 
   const CastCard({
-    required this.img,
+    required this.imgUrl,
     required this.role,
     required this.name,
     super.key,
@@ -21,7 +20,7 @@ class CastCard extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Container(
       padding: EdgeInsets.symmetric(
-        horizontal: size.width * 0.025,
+        horizontal: size.width * 0.03,
         vertical: size.height * 0.012,
       ),
       decoration: BoxDecoration(
@@ -29,15 +28,42 @@ class CastCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(16),
       ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          img,
-          Text(
-            '${AppLocalizations.of(context)!.name}: BLANK',
-            style: AppTextStyles.regular20White,
+          Image.network(
+            imgUrl,
+            fit: BoxFit.cover,
+            errorBuilder: (context, error, stackTrace) {
+              return Container(
+                height: size.height * 0.07,
+                color: AppColors.grey,
+                alignment: Alignment.center,
+                child: Icon(Icons.photo, color: AppColors.white, size: 50),
+              );
+            },
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress == null) return child;
+              return Container(
+                color: AppColors.grey,
+                child: Center(
+                  child: CircularProgressIndicator(color: AppColors.yellow),
+                ),
+              );
+            },
           ),
-          Text(
-            '${AppLocalizations.of(context)!.character}: BLANK',
-            style: AppTextStyles.regular20White,
+          Column(
+            children: [
+              SizedBox(width: size.width * 0.01),
+              Text(
+                '${AppLocalizations.of(context)!.name}: $role',
+                style: AppTextStyles.regular20White,
+                textAlign: TextAlign.start,
+              ),
+              Text(
+                '     ${AppLocalizations.of(context)!.character}: $name',
+                style: AppTextStyles.regular20White,
+              ),
+            ],
           ),
         ],
       ),
